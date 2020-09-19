95e2872e7b3ee17cc756294f96a0146662443773
dildeolupbiten <tckutlu@gmail.com>
2020-09-20 02:42:24 +0300

    Version is changed to 1.0.2.

--- Scripts/about.py
+++ Scripts/about.py
@@ -7,7 +7,7 @@ class About(tk.Toplevel):
     def __init__(self):
         super().__init__()
         self.resizable(width=False, height=False)
-        self.version = "1.0.1"
+        self.version = "1.0.2"
         self.name = "TkEnneagram"
         self.date_built = "10.09.2020"
         self.date_updated = dt.now().strftime("%d.%m.%Y")
